816c542e40d2ca71b587398391b40ed7d2b3c4d2
Sanket Shinde <70633400+ssankey@users.noreply.github.com>
2023-06-11 18:00:57 +0530

    google

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ export default function RootLayout({ children }) {
 return (
     
         <html lang="en">
+          <meta name="google-site-verification" content="yT-DPUsczh6YEnnHuPrvjrjXiqRa5e_bkSfpzoR_ZiQ" />
           <body>
           <Provider>
             {children}
